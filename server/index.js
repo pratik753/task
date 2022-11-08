06797f401004fdeff31cce571ca4c3fb27ab390a
pratik753 <60500551+pratik753@users.noreply.github.com>
2022-11-08 08:33:28 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://moviestar:pratik1234@cluster0.z2fm3.mongodb.net/moviestar?retryWrites=true&w=majority";
+// "mongodb+srv://pratik852:pratik852@cluster0.wzlh6.mongodb.net/pratik";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
